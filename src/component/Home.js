b7e9494a6349debf5ac1d88d95315e9814b11832
이 필 제 <xxpiiiide@gmail.com>
2021-05-27 19:02:26 +0900

    기본컴포넌트분류

---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import List from '../component/List';
+
+function Home() {
+	return (
+		<>
+			<h1>PIDE TODOLIST</h1>
+			<input></input>
+			<button> + 추가</button>
+			<List />
+		</>
+	);
+}
+
+export default Home;
