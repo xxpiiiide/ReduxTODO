07042b98e297becdc4d597322359728fc1b890e0
이 필 제 <xxpiiiide@gmail.com>
2021-06-09 08:27:11 +0900

    redux 작업

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Subcribers from './component/Subscribers';
 import '../src/App.css';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
 
 function App() {
 	return (
-		<div className="App">
-			<Subcribers />
-		</div>
+		<Provider store={store}>
+			<div className="App">
+				<Subcribers />
+			</div>
+		</Provider>
 	);
 }
 
