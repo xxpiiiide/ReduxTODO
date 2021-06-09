952fd08b323945f9029084bc6149ac0022a777e7
이 필 제 <xxpiiiide@gmail.com>
2021-06-09 18:19:28 +0900

    add: redux-devtoole-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
 
 const middleware = [logger]; //middleware가 여러개가 될 수 있기 때문에 선언해준다.
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware));
 
 export default store;
