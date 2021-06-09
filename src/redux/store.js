d7814e927a8dc0a9ff3e8e51d2d66538e2f4b352
이 필 제 <xxpiiiide@gmail.com>
2021-06-09 18:23:03 +0900

    add: redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
 
 
-
-const middleware = [logger]; //middleware가 여러개가 될 수 있기 때문에 선언해준다.
+const middleware = [logger, thunk]; //middleware가 여러개가 될 수 있기 때문에 선언해준다.
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware));
 
