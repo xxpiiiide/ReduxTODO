5123f39355bf8ab3f6da563290dfa79a87a12c55
이 필 제 <xxpiiiide@gmail.com>
2021-06-23 11:49:27 +0900

    type.ts 생성

---
+++ src/redux/type.ts
@@ -0,0 +1,32 @@
+export const INSERT = 'INSERT';
+export const REMOVE = 'REMOVE';
+export const EDIT = 'EDIT';
+export const DONE = 'DONE';
+export const CHECKREMOVE = 'CHECKREMOVE';
+
+
+export interface insertTODOType {
+    type: typeof INSERT
+    text: string
+}
+
+export interface removeTODOType {
+    type: typeof REMOVE
+    id: number
+}
+
+export interface editTODOType {
+    type: typeof EDIT
+    text: string
+    id: number
+}
+
+export interface doneTODOType {
+    type: typeof DONE
+    id: number
+    done: boolean
+}
+
+export interface checkRemoveType {
+    type: typeof CHECKREMOVE
+}
